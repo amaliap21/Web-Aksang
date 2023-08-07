663ede55b0f3d62b60183d238a6f0addf4795aa5
Intermaze <rafikiprawhira@gmail.com>
2023-08-07 17:07:50 +0700

    feat(basket): Added structure, textbox

---
+++ app/basket/page.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+function basket() {
+  return (
+    <main>
+      <div className='bg-[#092079] min-h-screen font-montserrat'>
+        <h1>
+          Basket
+        </h1>
+
+        <h2>
+          Ketentuan
+        </h2>
+        <div className='flex flex-col gap-4 '>
+          <p className="bg-white/30 justify-center align-middle px-6 py-4 w-2/5 rounded-2xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+          <p className="bg-white/30 justify-center align-middle px-6 py-4 w-2/5 rounded-2xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+          <p className="bg-white/30 justify-center align-middle px-6 py-4 w-2/5 rounded-2xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+          <p className="bg-white/30 justify-center align-middle px-6 py-4 w-2/5 mb-16 rounded-2xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
+
+        <div className='flex gap-10'>
+        <p className="bg-gradient-to-tl from-[#9999ff00] to-[#9999FF] justify-center align-middle px-6 py-4 w-2/5 rounded-2xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+          <p className="bg-gradient-to-tr from-[#9999ff00] to-[#9999FF] justify-center align-middle px-6 py-4 w-2/5 rounded-2xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
+
+      </div>
+    </main>
+    
+  )
+}
+
+export default basket
