46d8bb937b773933d6887691f5663e8b2769f49d
amaliap21 <116664738+amaliap21@users.noreply.github.com>
2023-08-09 17:34:00 +0700

    Revert "Merge branch 'home' of https://github.com/amaliap21/Web-Aksang into develop"
    This reverts commit 668fdd5c9666252c6e2a4c5859a0477ddffcb4c9, reversing
    changes made to a9e35ff9b251dd031aa75df2526537f9ac61d245.

--- app/page.js
+++ app/page.js
@@ -1,51 +1,41 @@
-import Hero from "@/components/hero";
-import About from "@/components/about";
-import Event from "@/components/event";
-import React from "react";
 import IconCard from "@/components/icon-card";
-const Home = () => {
+
+export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-20 p-10 lg:p-24 bg-custom-blue">
-      <Hero />
-      <About />{" "}
-      <section className="flex flex-col items-center justify-center">
-        <h2 className="text-[28px] lg:text-5xl text-white font-montserrat font-bold">
-          <span className="text-custom-yellow">Our </span>Event
-        </h2>
-        <div className="flex flex-wrap items-center justify-center w-full gap-x-60 2xl:gap-x-80 gap-y-20 lg:gap-y-16 mt-16 md:mt-20 lg:mt-32">
-          <IconCard
-            title="ByteTech Summit"
-            description="Talkshow bersama alumni HMIF dengan tema “Nilai Etika dan Sikap sebagai Informatikawan”"
-            buttonText="KUNJUNGI"
-            buttonUrl="/"
-            imageAlt="Cup"
-            imageUrl="/mini-robot.png"
-          />
-          <IconCard
-            title="Cup"
-            description="Pertandingan basket dan futsal antar angkatan di HMIF"
-            buttonText="KUNJUNGI"
-            buttonUrl="/"
-            imageAlt="Peluit"
-            imageUrl={"/peluit.png"}
-          />
-          <IconCard
-            title="ByteTech Summit"
-            description="Talkshow bersama alumni HMIF dengan tema “Nilai Etika dan Sikap sebagai Informatikawan”"
-            buttonText="KUNJUNGI"
-            buttonUrl="/"
-            imageAlt="zoom"
-            width={150}
-            height={150}
-            imageUrl={"/zoom.png"}
-          />
-        </div>
-      </section>
+    <main className="flex min-h-screen flex-col items-center justify-center p-10 lg:p-24 bg-custom-blue">
+     <h2 className="text-[28px] lg:text-5xl text-white font-montserrat font-bold"><span className="text-custom-yellow">Our </span>Event</h2>
+      <div className="flex flex-wrap items-center justify-center w-full gap-x-60 2xl:gap-x-80 gap-y-20 lg:gap-y-16 mt-16 md:mt-20 lg:mt-32">
+        <IconCard
+          title="ByteTech Summit"
+          description="Talkshow bersama alumni HMIF dengan tema “Nilai Etika dan Sikap sebagai Informatikawan”"
+          buttonText="KUNJUNGI"
+          buttonUrl="/"
+          imageAlt="Cup"
+          imageUrl="/mini-robot.png"
+        />
+        <IconCard
+          title="Cup"
+          description="Pertandingan basket dan futsal antar angkatan di HMIF"
+          buttonText="KUNJUNGI"
+          buttonUrl="/"
+          imageAlt="Peluit"
+          imageUrl={"/peluit.png"}
+        />
+        <IconCard
+          title="ByteTech Summit"
+          description="Talkshow bersama alumni HMIF dengan tema “Nilai Etika dan Sikap sebagai Informatikawan”"
+          buttonText="KUNJUNGI"
+          buttonUrl="/"
+          imageAlt="zoom"
+          width={150}
+          height={150}
+          imageUrl={"/zoom.png"}
+        />
+      </div>
     </main>
   );
-};
+}
 
-export default Home;
 export const metadata = {
   title: "Byte",
   description: "Website aksi angkatan Byte",
