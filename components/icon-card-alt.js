3f7a6e6716cb4452d10c096230e3315c28185b7e
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-08-10 00:23:33 +0700

    First Version (#12)
    * feat(hero): Change font size
    
    * feat(hero); Test deployment
    
    * feat(hero): Test error
    
    * Navbar
    
    * feat(hero): Hello & About
    
    * feat(basket): Added structure, textbox
    
    * feat(hero): Home Page
    
    * feat(hero)
    
    * feat(hero): Typografi
    
    * feat(basket): Title shadow, images, div daftar
    
    * fix(basket): fixed background, margin
    
    * feat(basket): Create reusable component for persuasive QR
    
    * fix(basket): Fix import next image problem on persuasive qr component
    
    * feat(home): add SVGs for home page
    
    * Navbar (#3)
    
    * feat(navbar): Create navbar component, add custom colors and fonts in tailwind config
    
    * feat(navbar): Add chevron icon
    
    * feat(navbar): Add animation onhover sublinks, make responsive on any devices
    
    * Footer (#5)
    
    * feat(footer): Create footer, add responsive for any devices, and implement as layout with navbar component
    
    * fix(footer): Fix formatting standar eslint for vercel deployment
    
    * fix(footer): Fix rendering import font
    
    * fix(footer): Fix meta data generated on server component
    
    * Icon cards (#6)
    
    * feat(icon-cards): Add image for building requirements
    
    * feat(icon-cards): Create icon cards compoent, and give the sample code on homepage
    
    * feat(hero): add images and SVGs
    
    * feat(home): adding SVGs PNGs, abou,t, hero
    
    * Revert "Merge branch 'develop' of https://github.com/amaliap21/Web-Aksang into develop"
    
    This reverts commit 86c0f1bcb2b15046c22804fb9900122d2d75c7cf, reversing
    changes made to 0f4ff5b2355de38019207801cf58b758da74e2b0.
    
    * Revert "Revert "Merge branch 'develop' of https://github.com/amaliap21/Web-Aksang into develop""
    
    This reverts commit a282dd13eb67679d2b2ddc51e6867ecc5b27bb1b.
    
    * Revert "Merge branch 'home' of https://github.com/amaliap21/Web-Aksang into develop"
    
    This reverts commit 668fdd5c9666252c6e2a4c5859a0477ddffcb4c9, reversing
    changes made to a9e35ff9b251dd031aa75df2526537f9ac61d245.
    
    * 404 (#7)
    
    * feat(404): Create 404 page and add responsive design
    
    * feat(404): Change image robot for 404 page
    
    * feat(404): Change image robot for 404 page
    
    * feat(404): Change design on desktop 2xl version
    
    * Basket (#4)
    
    * feat(basket): Changed basket image
    
    * feat(basket): Basket blur, background elements, and more
    
    * fix(basket): fixed font not applying
    
    * feat(basket): mobile responsive
    
    * feat(basket): Added metadata
    
    * feat(basket): added cup folder
    
    * feat(basket): Add conditional clor on chevron basket
    
    ---------
    
    Co-authored-by: Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
    
    * Futsal (#11)
    
    * feat(basket): Changed basket image
    
    * feat(basket): Basket blur, background elements, and more
    
    * fix(basket): fixed font not applying
    
    * feat(basket): mobile responsive
    
    * feat(futsal): copied basket, added metadata
    
    * feat(futsal): added cup folder
    
    ---------
    
    Co-authored-by: amaliap21 <116664738+amaliap21@users.noreply.github.com>
    Co-authored-by: Intermaze <rafikiprawhira@gmail.com>
    Co-authored-by: Rafiki Prawhira Harianto <74698105+Intermaze@users.noreply.github.com>

---
+++ components/icon-card-alt.js
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const IconCardAlt = ({
+  title,
+  description,
+  buttonText,
+  buttonUrl,
+  imageUrl,
+  imageAlt,
+  width = 200,
+  height = 200,
+}) => {
+  return (
+    <div className={`w-full sm:w-fit sm:max-w-[430px] lg:min-h-[260px] text-white px-10 pt-[75px] pb-12 lg:pt-[83px] lg:pb-7 font-montserrat items-center rounded-lg bg-gradient-to-tr from-[#0f257f] to-[#3646a3] relative flex flex-col gap-2`}
+    >
+      <Image
+        src={imageUrl}
+        alt={imageAlt}
+        width={width}
+        height={height}
+        className={`absolute w-[120px] h-[100px] lg:w-[${width}px] lg:h-[${height}px] object-center object-contain right-1/2 translate-x-1/2 -top-10 lg:bottom-[185px] `}
+      />
+      <div className="flex flex-col flex-1 justify-between gap-3">
+        <div className="flex flex-col items-center justify-start gap-2">
+          <h3 className="text-3xl lg:text-4xl text-center font-bold capitalize">{title}</h3>
+          <p className="text-base lg:text-lg text-center">{description}</p>
+        </div>
+        {buttonText && 
+        <button
+          style={{
+            background:
+              "linear-gradient(270deg, #7721D6 0%, rgba(119, 33, 214, 0.40) 100%)",
+          }}
+          className="text-center rounded-2xl px-10 font-semibold font-montserrat py-0.5"
+        >
+          <Link href={`${buttonUrl}`}>{buttonText}</Link>
+        </button>
+          }
+      </div>
+    </div>
+  );
+};
+
+export default IconCardAlt;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+        montserrat: ["var(--font-montserrat)"],
       },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors:{
+        "custom-blue-dark":"#0D0151",
+        "custom-pink":"#710C50",
+        "custom-blue":"#092079",
+        "custom-yellow":"#FADE7A",
+      }
     },
   },
   plugins: [],
-}
+};
