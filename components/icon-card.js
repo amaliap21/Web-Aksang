848f1e6253095387d6e1d2f7cdb049f1683a438c
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-08-09 12:13:13 +0700

    Icon cards (#6)
    * feat(icon-cards): Add image for building requirements
    
    * feat(icon-cards): Create icon cards compoent, and give the sample code on homepage

--- components/icon-card.js
+++ components/icon-card.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-const IconCard = () => {
+const IconCard = ({
+  title,
+  description,
+  buttonText,
+  buttonUrl,
+  imageUrl,
+  imageAlt,
+  width = 200,
+  height = 200,
+}) => {
   return (
-    <div className="max-w-[430px] relative flex flex-col gap-2">
-      
+    <div
+      style={{
+        background:
+          "linear-gradient(148deg, rgba(255, 255, 255, 0.25) 0%, rgba(255, 255, 255, 0.10) 120%)",
+      }}
+      className="w-full sm:w-fit sm:max-w-[430px] lg:min-h-[260px] text-white px-10 pt-[75px] pb-12 lg:pt-[83px] lg:pb-7 font-montserrat items-center rounded-lg bg-gradient-to-br from-[##FFFFFF1A] to-[#FFFFFF40] relative flex flex-col gap-2"
+    >
+      <Image
+        src={imageUrl}
+        alt={imageAlt}
+        width={width}
+        height={height}
+        className={`absolute w-[120px] h-[100px] lg:w-[${width}px] lg:h-[${height}px] object-center object-contain right-1/2 translate-x-1/2 -top-10 lg:bottom-[185px] `}
+      />
+      <div className="flex flex-col flex-1 justify-between gap-3">
+        <div className="flex flex-col items-center justify-start gap-2">
+          <h3 className="text-xl lg:text-2xl text-center font-bold capitalize">{title}</h3>
+          <p className="text-sm lg:text-base text-center">{description}</p>
+        </div>
+        <button
+          style={{
+            background:
+              "linear-gradient(270deg, #7721D6 0%, rgba(119, 33, 214, 0.40) 100%)",
+          }}
+          className="text-center rounded-2xl px-10 font-semibold font-montserrat py-0.5"
+        >
+          <Link href={`${buttonUrl}`}>{buttonText}</Link>
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default IconCard
+export default IconCard;
