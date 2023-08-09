78a59dfae2d161fdaeb0f29a54e5ff11cd8ad80c
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-08-09 07:46:25 +0700

    fix(footer): Fix meta data generated on server component

---
+++ components/icon-card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IconCard = () => {
+  return (
+    <div className="max-w-[430px] relative flex flex-col gap-2">
+      
+    </div>
+  )
+}
+
+export default IconCard
