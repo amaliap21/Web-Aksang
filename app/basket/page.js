210d730ab49540f58dc2d9ff5ec1855715d10102
Intermaze <rafikiprawhira@gmail.com>
2023-08-09 22:45:15 +0700

    feat(basket): mobile responsive

--- app/basket/page.js
+++ app/basket/page.js
@@ -6,56 +6,62 @@ import React from 'react'
 function basket() {
 
   const arrayKetentuan = [
-    "Lorem ipsum dolor sit amet consectetur. Odio urna nunc orci mus feugiat convallis amet vitae. Odio urna nunc orci mus feugiat convallis amet vitae.",
-    "Lorem ipsum dolor sit amet consectetur. Odio urna nunc orci mus feugiat convallis amet vitae. Odio urna nunc orci mus feugiat convallis amet vitae.",
-    "Lorem ipsum dolor sit amet consectetur. Odio urna nunc orci mus feugiat convallis amet vitae. Odio urna nunc orci mus feugiat convallis amet vitae.",
-    "Lorem ipsum dolor sit amet consectetur. Odio urna nunc orci mus feugiat convallis amet vitae. Odio urna nunc orci mus feugiat convallis amet vitae.",
-    "Lorem ipsum dolor sit amet consectetur. Odio urna nunc orci mus feugiat convallis amet vitae. Odio urna nunc orci mus feugiat convallis amet vitae.",
+    "Lorem ipsum dolor sit amet consectetur. Odio urna nunc orci mus feugiat convallis amet vitae.",
+    "Lorem ipsum dolor sit amet consectetur. Odio urna nunc orci mus feugiat convallis amet vitae.",
+    "Lorem ipsum dolor sit amet consectetur. Odio urna nunc orci mus feugiat convallis amet vitae.",
+    "Lorem ipsum dolor sit amet consectetur. Odio urna nunc orci mus feugiat convallis amet vitae.",
+    "Lorem ipsum dolor sit amet consectetur. Odio urna nunc orci mus feugiat convallis amet vitae.",
   ]
 
   return (
-    <main className=' font-montserrat'>
+    <main className=' font-montserrat bg-[#092079] min-h-screen overflow-hidden'>
 
-      <div className='bg-[#092079] min-h-screen overflow-hidden'>
+      
+      <div className='px-8 lg:px-12'>
+        <Image src="/piala.png" alt="Piala Background" width={286} height={414} className=' w-[130px] lg:w-[286px] absolute left-0 top-0 lg:top-0'/>
 
         <div className='relative'>
-          <Image src="/garis_left.png" alt="Garis Background" width={591} height={565} className='absolute -right-40 -top-40' />
-          <Image src="/piala.png" alt="Piala Background" width={286} height={414} className='absolute left-0 -top-10'/>
-          <Image src="/garis_up.png" alt="Garis Background" width={478} height={591} className='absolute left-0 top-[940px]'/>
+          <Image src="/garis_left.png" alt="Garis Background" width={591} height={565} className='absolute -right-40 -top-40 hidden lg:block'/>
+          <Image src="/garis_up.png" alt="Garis Background" width={478} height={591} className='absolute left-0 top-[940px] hidden lg:block'/>
         </div>
 
-        <h1 className='pt-[111px] mb-[100px] flex justify-center font-bold drop-shadow-md text-[54px] [text-shadow:_0px_4px_4px_#6D459A]  leading-[140%] tracking-[0.54px]'  >
+        <h1 className='pt-[60px] lg:pt-[111px] mb-[60px] lg:mb-[100px] flex justify-center font-bold drop-shadow-md text-[28px] lg:text-[54px] [text-shadow:_0px_4px_4px_#6D459A] leading-[140%] tracking-[0.54px]'  >
           <span className="text-custom-yellow">Byte </span> &nbsp;Basketball
         </h1>
 
-        <div className='flex mb-[200px] justify-center'>
-          <div className='flex flex-col gap-[24px] mr-[32px]' >
-            <h2 className='text-custom-yellow text-[36px] font-bold'>
+        <div className='flex mb-[120px] lg:mb-[200px] justify-center'>
+          <div className='flex flex-col gap-[20px] lg:gap-[24px] lg:mr-[32px]' >
+            <h2 className='text-custom-yellow text-[24px] lg:text-[36px] font-bold'>
               Ketentuan
             </h2>
             {arrayKetentuan.map((text) => (
-            <p key="index" className="bg-white/30 justify-center align-middle px-6 py-4 max-h-[98px] max-w-[654px] rounded-2xl text-justify">
+            <p key="index" className="bg-white/30 justify-center align-middle px-6 py-4 min-h-[98px] max-w-[654px] rounded-2xl text-justify text-[12px] lg:text-base">
               {text}
             </p>
             ))}
           </div>
-          <Image src="/bola_basket.png" alt="Bola Basket" width={500} height={500} className='self-center'
+
+          <Image src="/bola_basket.png" alt="Bola Basket" width={500} height={500} className='self-center w-[150px] lg:w-[450px]'
           style={{
-            background: "radial-gradient(circle, rgb(47, 67, 142) 170px, rgba(0,0,0,0) 250px",
-          }}/>
+            background: "radial-gradient(circle, rgb(47, 67, 142) 55%, rgba(0,0,0,0) 71%"
+            }
+          }/>
         </div>
 
-        <div className='flex gap-[71px] justify-center mb-[120px]'>
+        <div className='flex flex-col lg:flex-row gap-14 lg:gap-[71px] justify-center items-center mb-[80px] lg:mb-[120px]'>
           <IconCardAlt title={"Waktu"} description={"waktu basket waktu basket waktu basket waktu basket"} imageUrl={"/waktu.png"} imageAlt={"Logo Jam"} width={170} height={152}/>
           <IconCardAlt title={"Tempat"} description={"tempat basket tempat basket tempat basket tempat basket "} imageUrl={"/tempat.png"} imageAlt={"LogoTempat"} width={112} height={170}/>
         </div>
-       <PersuasiveQr 
+ 
+
+      </div>
+
+      <PersuasiveQr 
        iconLeftUrl="/bell.png" 
        yellowText="Tunggu Apalagi," 
        whiteText="Segera daftarkan tim kamu!!" 
        qrImageUrl="/qr_code.png" 
        buttonText="DAFTAR DISINI"/>
-      </div>
 
     </main>
     
--- components/persuasive-qr.js
+++ components/persuasive-qr.js
@@ -2,23 +2,25 @@ import Image from 'next/image'
 
 export default function PersuasiveQr({yellowText, whiteText, iconLeftUrl, qrImageUrl, buttonText}){
   return (
-    <div className=' flex mt-8 bg-[#9999FF]/50 rounded-tr-[120px] min-h-[400px] justify-center'>
-      <div className='ml-[38px] my-auto'>
-        <Image src={iconLeftUrl} alt="Bell" width={183} height={150}/>
-      </div>
-      <div className='ml-[30px] my-auto'>
-        <h2 className='text-[54px] font-bold '>
-          <span className="text-custom-yellow">{yellowText} </span> <br />
-        </h2>
-        <h2 className='text-[54px] font-bold indent-12'>
-          {whiteText}
-        </h2>
-      </div>
-      <div className='flex flex-col my-auto pl-12'>
-        <div className='mx-auto'>
-          <Image src={qrImageUrl} alt="QR Code" width={152} height={150} />
+    <div className=' flex flex-col lg:flex-row mt-8 bg-[#9999FF]/50 rounded-tr-[80px] lg:rounded-tr-[120px] min-h-[290px] lg:min-h-[400px] justify-center'>
+      <div className='flex flex-row mt-[14px] mb-0 lg:my-auto'>
+        <div className='ml-[14px] lg:ml-[38px]  w-[60px] lg:w-[183px]'>
+          <Image src={iconLeftUrl} alt="Bell" width={183} height={150}/>
         </div>
-        <button className='text-[16px] bg-[#C53998] border-[#9999FF] mt-[20px] font-semibold py-[20px] px-[50px] rounded-2xl border drop-shadow-xl'>
+        <div className='ml-[6px] lg:ml-[30px] '>
+          <h2 className='text-[20px] lg:text-[54px] font-bold '>
+            <span className="text-custom-yellow">{yellowText} </span> <br />
+          </h2>
+          <h2 className='text-[16px] lg:text-[54px] font-bold indent-12'>
+            {whiteText}
+          </h2>
+        </div>
+      </div>
+      <div className='flex flex-col my-auto lg:pl-12'>
+        <div className='mx-auto'>
+          <Image className="w-[100px] lg:w-[152px]" src={qrImageUrl} alt="QR Code" width={152} height={150} />
+        </div>
+        <button className='mx-auto w-[156px] h-[44px] lg:w-[226px] lg:h-[60px] text-[14px] lg:text-[16px] bg-[#C53998] border-[#9999FF] mt-[20px] font-semibold rounded-2xl border drop-shadow-xl '>
         {buttonText}
         </button>
       </div>
