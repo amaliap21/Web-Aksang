0a2fce9b582ca3ee3dbf03cf482670437716dc03
amaliap21 <116664738+amaliap21@users.noreply.github.com>
2023-08-10 00:56:23 +0700

    feat(develop): deskripsi event home page

--- app/page.js
+++ app/page.js
@@ -3,11 +3,13 @@ import IconCard from "@/components/icon-card-alt";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-10 lg:p-24 bg-custom-blue">
-     <h2 className="text-[28px] lg:text-5xl text-white font-montserrat font-bold"><span className="text-custom-yellow">Our </span>Event</h2>
+      <h2 className="text-[28px] lg:text-5xl text-white font-montserrat font-bold">
+        <span className="text-custom-yellow">Our </span>Event
+      </h2>
       <div className="flex flex-wrap items-center justify-center w-full gap-x-60 2xl:gap-x-80 gap-y-20 lg:gap-y-16 mt-16 md:mt-20 lg:mt-32">
         <IconCard
-          title="ByteTech Summit"
-          description="Talkshow bersama alumni HMIF dengan tema “Nilai Etika dan Sikap sebagai Informatikawan”"
+          title="Exhi(Byte)On"
+          description="Pameran teknologi yang mengangkat tema isu-isu global serta permasalahan di lingkungan kampus."
           buttonText="KUNJUNGI"
           buttonUrl="/"
           imageAlt="Cup"
@@ -23,7 +25,7 @@ export default function Home() {
         />
         <IconCard
           title="ByteTech Summit"
-          description="Talkshow bersama alumni HMIF dengan tema “Nilai Etika dan Sikap sebagai Informatikawan”"
+          description="Puncak acara yang mewadahi talkshow dengan topik seputar keprofesian di bidang informatika."
           buttonText="KUNJUNGI"
           buttonUrl="/"
           imageAlt="zoom"
