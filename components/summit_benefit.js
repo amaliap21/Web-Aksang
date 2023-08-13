639c2a08218af001cb51b74323728438cd0b3d31
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-08-13 22:02:28 +0700

    feat(summit): Change minimum double card on mobile device benefit

--- components/summit_benefit.js
+++ components/summit_benefit.js
@@ -4,7 +4,7 @@ const Benefit = (
   text = ''
 ) => {
     return(
-      <div className="box-border w-[175px] h-fit flex flex-col justify-center items-center gap-[20px]">
+      <div className="box-border w-[150px] h-fit flex flex-col justify-center items-center gap-[20px]">
         <div className="box-border w-32 lg:w-40 aspect-square rounded-[20px] bg-gradient-diagonals from-[#9999FF] via-[#9999FF9E] to-[#9999FF00] opacity-30"></div>
         <h2 className="text-[16px] lg:text-xl break-words text-center font-montserrat">{text.texts}</h2>
       </div>
