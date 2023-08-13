51da6b39528efae1c1314782aeaa74d7feabc6eb
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-08-13 21:40:06 +0700

    feat(summit): Change weight of font on benefit card

--- components/summit_benefit.js
+++ components/summit_benefit.js
@@ -6,7 +6,7 @@ const Benefit = (
     return(
       <div className="box-border w-[275px] h-fit flex flex-col justify-center items-center gap-[20px]">
         <div className="box-border w-40 h-[150px] rounded-[20px] bg-gradient-diagonals from-[#9999FF] via-[#9999FF9E] to-[#9999FF00] opacity-30"></div>
-        <h2 className="text-[16px] lg:text-xl break-words text-center font-montserrat font-semibold">{text.texts}</h2>
+        <h2 className="text-[16px] lg:text-xl break-words text-center font-montserrat">{text.texts}</h2>
       </div>
     )
   };
