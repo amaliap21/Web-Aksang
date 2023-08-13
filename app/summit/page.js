0ee116f63cdd37d5cef9982160cddbf4904631a1
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-08-13 21:27:02 +0700

    feat(summit): Fix the responsive design for any devices

--- app/summit/page.js
+++ app/summit/page.js
@@ -24,241 +24,162 @@ const textBenefit = [
 
 export default function SummitPage() {
   return (
-    <main className="bg-[#092079] min-h-screen overflow-hidden overflow-x-hidden relative flex flex-col">
-      {/* BG Image svg */}
-      <Image
-        src="/summitbg.svg"
-        alt="Background summit"
-        width={572}
-        height={572}
-        className="w-[500px] aspect-square absolute z-10 top-0 left-[-80px]"
-      ></Image>
-      <Image
-        src="/summitbg.svg"
-        alt="Background summit"
-        width={572}
-        height={572}
-        className="w-[400px] aspect-square absolute z-[3] top-[130px] right-[-20px] rotate-180 "
-      ></Image>
-      {/* TITLE */}
-      <div className="lg:mx-20 mx-10 pt-20 ">
-        <h1 className="montserrat font-bold relative text-[54px] text-white tracking-[0.54px] text-5xl text-center">
-          ByteTech <span className="text-[#FADE7A]">Summit</span>
-        </h1>
-        <h2 className="montserrat text-[36px] font-[500] p-2 text-white tracking-[0.72px] text-center">
-          Empowering Society’s Future Through Informatics
-        </h2>
-        <p className="py-10 px-3 tracking-[0.16px] text-[16px] text-white max-w-[1000px] font-[4vw] mx-auto text-center">
-          Ultricies mi quis hendrerit dolor. A pellentesque sit amet porttitor
-          eget dolor morbi. Nisl purus in mollis nunc sed id semper risus.
-          Semper quis lectus nulla at. Lorem ipsum dolor sit amet consectetur
-          adipiscing. Dui id ornare arcu odio ut. Fusce id velit ut tortor
-          pretium viverra suspendisse.
-        </p>
-      </div>
+    <main className="bg-[#092079] min-h-screen w-full overflow-hidden relative flex flex-col">
+      <div className="relative z-10 flex flex-col px-8 lg:px-12">
+        {/* BG Image svg */}
+        <Image
+          src="/summitbg.svg"
+          alt="Background summit"
+          width={572}
+          height={572}
+          className="w-[500px] z-[-1] aspect-square absolute top-0 left-[-80px]"
+        ></Image>
+        <Image
+          src="/summitbg.svg"
+          alt="Background summit"
+          width={572}
+          height={572}
+          className="w-[400px] aspect-square absolute z-[-3] top-[130px] right-[-20px] rotate-180 "
+        ></Image>
+        {/* TITLE */}
+        <div className="px-3 lg:px-20 2xl:px-48 pt-[60px] lg:pt-[120px] ">
+          <h1 className="font-montserrat font-bold relative text-[28px] lg:text-[54px] text-white tracking-[0.54px] text-5xl text-center">
+            ByteTech <span className="text-[#FADE7A]">Summit</span>
+          </h1>
+          <h2 className="font-montserrat text-lg lg:text-4xl font-medium mt-3 lg:mt-4 text-white tracking-[0.72px] text-center">
+            Empowering Society’s Future Through Informatics
+          </h2>
+          <p className="font-montserrat mt-10 mb-8 lg:my-14 px-3 text-base lg:text-xl text-white font-regular text-center">
+            Ultricies mi quis hendrerit dolor. A pellentesque sit amet porttitor
+            eget dolor morbi. Nisl purus in mollis nunc sed id semper risus.
+            Semper quis lectus nulla at. Lorem ipsum dolor sit amet consectetur
+            adipiscing. Dui id ornare arcu odio ut. Fusce id velit ut tortor
+            pretium viverra suspendisse.
+          </p>
+        </div>
 
-      {/* ABOUT SPEAKER MODIFIED */}
-      <div
-        className="flex items-center justify-center mx-auto rounded-[10px] max-w-[1200px] shadow xl: shadow-white/20 lg:h-[681px] lg:w-[1100px] md:w-[750px] lg:mx-auto"
-        style={{
-          background:
-            "linear-gradient(146deg, rgba(255, 255, 255, 0.60) 0%, rgba(255, 255, 255, 0.20) 100%)",
-        }}
-      >
-        {/* CONTAINER PUTIH */}
-        <div className="flex lg:flex-row m-5 gap-20 py-20 px-7 lg:px-20 bg-white w-[300px] md:w-[1050px] overflow-x-visible snap-both sm:h-[1250px] md:h-[600px] lg:h-[615px] rounded-lg overflow-y-hidden text-center snap-mandatory">
-          {/* CONTAINER TULISAN-KARTU */}
-          <div className="flex flex-col-reverse sm:flex-row items-center justify-center lg:flex-row flex-shrink-0 gap-10 snap-always snap-center">
-            {/* TULISAN-KIRI */}
-            <div className="flex flex-col lg:w-[535px] md:w-[400px] w-[250px] lg:h-[296px]">
-              <h2 className="text-[#092079] text-3xl montserrat font-[700] text-left text-[36px] pb-6">
-                ABOUT SPEAKER,
-              </h2>
-              <p className="text-[#092079] text-justify">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.{" "}
-              </p>
-            </div>
-            {/* KARTU SPEAKER */}
-            <div
-              className="flex flex-col lg:w-[325px] lg:h-[450px] rounded-xl shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]"
-              style={{
-                background: "linear-gradient(317deg, #092079 0%, #4F9BFD 100%)",
-              }}
-            >
-              <Image
-                src="/"
-                alt="Speaker"
-                width={500}
-                height={500}
-                className="lg:w-[350px] lg:h-[350px] w-[205px] h-[205px]"
-              />
-              <h2 className="text-white lg:text-[24px] tracking-[-0.48px] font-[700] montserrat py-5 items-center">
-                SPEAKER NAME
-              </h2>
-            </div>
-          </div>
-          <div className="flex flex-col-reverse items-center justify-center tmd:flex-row flex-shrink-0 gap-10 snap-always snap-center">
-            {/* TULISAN-KIRI */}
-            <div className="flex flex-col lg:w-[535px] md:w-[400px] w-[250px] lg:h-[296px]">
-              <h2 className="text-[#092079] text-3xl montserrat font-[700] text-left text-[36px] pb-6">
-                ABOUT SPEAKER,
-              </h2>
-              <p className="text-[#092079] text-justify">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.{" "}
-              </p>
-            </div>
-            {/* KARTU SPEAKER */}
-            <div
-              className="flex flex-col lg:w-[325px] lg:h-[450px] rounded-xl shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]"
-              style={{
-                background: "linear-gradient(317deg, #092079 0%, #4F9BFD 100%)",
-              }}
-            >
-              <Image
-                src="/"
-                alt="Speaker"
-                width={500}
-                height={500}
-                className="lg:w-[350px] lg:h-[350px] w-[205px] h-[205px]"
-              />
-              <h2 className="text-white lg:text-[24px] tracking-[-0.48px] font-[700] montserrat py-5 items-center">
-                SPEAKER NAME
-              </h2>
-            </div>
-          </div>
-          <div className="flex flex-col-reverse sm:flex-row items-center justify-center lg:flex-row flex-shrink-0 gap-10 snap-always snap-center">
-            {/* TULISAN-KIRI */}
-            <div className="flex flex-col lg:w-[535px] md:w-[400px] w-[250px] lg:h-[296px]">
-              <h2 className="text-[#092079] text-3xl montserrat font-[700] text-left text-[36px] pb-6">
-                ABOUT SPEAKER,
-              </h2>
-              <p className="text-[#092079] text-justify">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.{" "}
-              </p>
-            </div>
-            {/* KARTU SPEAKER */}
-            <div
-              className="flex flex-col lg:w-[325px] lg:h-[450px] rounded-xl shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]"
-              style={{
-                background: "linear-gradient(317deg, #092079 0%, #4F9BFD 100%)",
-              }}
-            >
-              <Image
-                src="/"
-                alt="Speaker"
-                width={500}
-                height={500}
-                className="lg:w-[350px] lg:h-[350px] w-[205px] h-[205px]"
-              />
-              <h2 className="text-white lg:text-[24px] tracking-[-0.48px] font-[700] montserrat py-5 items-center">
-                SPEAKER NAME
-              </h2>
+        {/* ABOUT SPEAKER MODIFIED */}
+        <div
+          style={{
+            background:
+              "linear-gradient(146deg, rgba(255, 255, 255, 0.60) 0%, rgba(255, 255, 255, 0.20) 100%)",
+          }}
+          className="flex w-full sm:w-[calc(100%-100px)] sm:mx-auto items-center border drop-shadow-[10px_20px_90px_#1c4a79] border-white p-4 lg:p-6 justify-center rounded-lg shadow xl:shadow-white/20 lg:max-w-[1100px] md:max-w-[750px]"
+        >
+          {/* CONTAINER PUTIH */}
+          <div className="flex lg:flex-row font-montserrat py-10 px-5 md:p-10 lg:p-16 bg-white w-full md:max-w-[1050px] items-center justify-center rounded-lg overflow-hidden text-center">
+            {/* CONTAINER TULISAN-KARTU */}
+
+            <div className="flex flex-col-reverse gap-7 lg:gap-10 items-center justify-center lg:flex-row ">
+              {/* TULISAN-KIRI */}
+              <div className="flex flex-col md:flex-1 lg:max-w-[535px] md:max-w-[600px] px-1 sm:px-4 lg:px-0 w-full">
+                <h2 className="text-[#092079] text-xl lg:text-3xl montserrat font-bold text-left pb-6">
+                  ABOUT SPEAKER,
+                </h2>
+                <p className="text-[#092079] text-sm lg:text-base text-justify">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum. Lorem ipsum dolor sit amet,
+                  consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                  ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+                  ea commodo consequat. Duis aute irure dolor in reprehenderit
+                  in voluptate velit esse cillum dolore eu fugiat nulla
+                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+                  in culpa qui officia deserunt mollit anim id est laborum.{" "}
+                </p>
+              </div>
+              {/* KARTU SPEAKER */}
+              <div
+                className="flex flex-col h-[270px] lg:w-[325px] lg:h-[440px] items-center rounded-xl shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]"
+                style={{
+                  background:
+                    "linear-gradient(317deg, #092079 0%, #4F9BFD 100%)",
+                }}
+              >
+                <Image
+                  src="/tes-thea.jpg"
+                  alt="Speaker"
+                  width={500}
+                  height={500}
+                  className="lg:w-[350px] lg:h-[350px] object-cover object-center w-[205px] h-[205px]"
+                />
+                <h2 className="text-white text-base lg:text-2xl tracking-[0.48px] my-auto font-semibold font-montserrat items-center">
+                  SPEAKER NAME
+                </h2>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      {/* Mengapa Harus Join Summit? */}
-      <Image
-        src="/summitbgv2.svg"
-        alt="Background summit2"
-        width={572}
-        height={572}
-        className="w-[500px] aspect-square absolute z-10 bottom-[700px] left-[-80px]"
-      ></Image>
-      <div class="flex item-center justify-center my-[100px]">
-        <div class="w-fit max-w-[80%] flex justify-center flex-wrap">
-          <div class="mx-auto my-[50px] self-center z-0 flex justify-center align-center">
-            <Image
-              src={"/giftNc.png"}
-              alt="Gift"
-              width={620}
-              height={630}
-              class="max-w-[80%]"
-            ></Image>
-          </div>
-          <div class="flex flex-col gap-[60px] mx-auto max-w-[100%]">
-            <h1 class=" md:w-4/6 break-words text-left font-montserrat text-white font-bold text-[24px] md:text-[36px]">
-              Mengapa <span class="text-custom-yellow">Harus Join Byte</span>{" "}
-              Summit?
-            </h1>
+        {/* Mengapa Harus Join Summit? */}
+        <Image
+          src="/summitbgv2.svg"
+          alt="Background summit2"
+          width={572}
+          height={572}
+          className="w-[500px] aspect-square absolute z-[-4] bottom-[700px] left-[-80px]"
+        ></Image>
+        <div className="flex item-center justify-center my-[60px] lg:my-[80px] 2xl:my-[100px]">
+          <div className="w-fit max-w-[80%] lg:gap-20 flex justify-center flex-wrap">
+            <div className="mx-auto mb-[50px] lg:my-auto self-center z-0 flex justify-center align-center">
+              <Image
+                src={"/giftNc.png"}
+                alt="Gift"
+                width={620}
+                height={630}
+                className="w-[250px] md:w-[400px] 2xl:w-[550px]"
+              ></Image>
+            </div>
+            <div className="flex flex-col gap-10 lg:gap-[60px] mx-auto">
+              <h1 className=" md:w-4/6 break-words text-left font-montserrat text-white font-bold text-xl lg:text-4xl">
+                Mengapa{" "}
+                <span className="text-custom-yellow">Harus Join Byte</span>{" "}
+                Summit?
+              </h1>
 
-            <div class="box-border md:w-[675px] gap-[20px] md:gap-[60px] text-white flex flex-wrap justify-center">
-              {textBenefit.map((text, index) => (
-                <Benefit {...text} key={index} />
-              ))}
+              <div className="box-border md:w-[675px] gap-[20px] md:gap-[60px] text-white flex flex-wrap justify-center">
+                {textBenefit.map((text, index) => (
+                  <Benefit {...text} key={index} />
+                ))}
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      {/* Other Information Section */}
-      <div className="flex flex-col lg:flex-row gap-14 lg:gap-[71px] justify-center items-center mb-[80px] lg:mb-[120px]">
-        {/* Icon Card for Waktu */}
-        <IconCardAlt
-          title={"Waktu"}
-          description={"Ini waktu Ini waktu Ini waktu Ini waktu Ini waktu"}
-          imageUrl={"/waktu.png"}
-          imageAlt={"Logo Jam"}
-          width={170}
-          height={152}
-        />
-        {/* Icon Card for Tempat */}
-        <IconCardAlt
-          title={"Tempat"}
-          description={"Ini tempat Ini tempat Ini tempat Ini tempat"}
-          imageUrl={"/tempat.png"}
-          imageAlt={"LogoTempat"}
-          width={112}
-          height={170}
-        />
+        {/* Other Information Section */}
+        <div className="flex flex-col lg:flex-row gap-14 mt-[40px] lg:mt-[50px] lg:gap-[71px] justify-center items-center mb-[80px] lg:mb-[120px]">
+          {/* Icon Card for Waktu */}
+          <IconCardAlt
+            title={"Waktu"}
+            description={"Ini waktu Ini waktu Ini waktu Ini waktu Ini waktu"}
+            imageUrl={"/waktu.png"}
+            imageAlt={"Logo Jam"}
+            width={170}
+            height={152}
+          />
+          {/* Icon Card for Tempat */}
+          <IconCardAlt
+            title={"Tempat"}
+            description={"Ini tempat Ini tempat Ini tempat Ini tempat"}
+            imageUrl={"/tempat.png"}
+            imageAlt={"LogoTempat"}
+            width={112}
+            height={170}
+          />
+        </div>
       </div>
-
       {/* Persuasive QR Section */}
       <PersuasiveQr
         iconLeftUrl="/bell.png"
         yellowText="Tunggu Apalagi,"
         whiteText="Segera daftarkan dirimu di Summit!!"
-        qrImageUrl="/"
+        qrImageUrl="/qr_code.png"
         buttonText="DAFTAR DISINI"
         linkUrl="/"
       />
--- components/summit_benefit.js
+++ components/summit_benefit.js
@@ -4,9 +4,9 @@ const Benefit = (
   text = ''
 ) => {
     return(
-      <div class="box-border w-[275px] h-fit flex flex-col justify-center items-center gap-[20px]">
-        <div class="box-border w-40 h-[150px] rounded-[20px] bg-gradient-diagonals from-[#9999FF] via-[#9999FF9E] to-[#9999FF00] opacity-30"></div>
-        <h2 class="text-[16px] break-words text-center font-montserrat font-semibold">{text.texts}</h2>
+      <div className="box-border w-[275px] h-fit flex flex-col justify-center items-center gap-[20px]">
+        <div className="box-border w-40 h-[150px] rounded-[20px] bg-gradient-diagonals from-[#9999FF] via-[#9999FF9E] to-[#9999FF00] opacity-30"></div>
+        <h2 className="text-[16px] lg:text-xl break-words text-center font-montserrat font-semibold">{text.texts}</h2>
       </div>
     )
   };
